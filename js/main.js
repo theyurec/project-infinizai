962406db86470e23a524bd8e3254cc43a3f1bfa8
Yuriy <the.yurec1@yandex.ru>
2022-11-12 22:00:15 +0300

    header

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-$('.burger').on('click', function(e) {
-    e.preventDefault()
-    $('.header__top').toggleClass('header__top--open')
-    $('.burger').toggleClass('burger--close')
-
-})
+$(function () {
+    $('.burger').on('click', function(e) {
+        e.preventDefault()
+        $('.header__top').toggleClass('header__top--open')
+        $('.burger').toggleClass('burger--close')
+    
+    })
+    setInterval(() => {
+        if($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+            $('.burger').addClass('burger--follow')
+        } else {
+            $('.burger').removeClass('burger--follow')
+        }
+    },0)
+});
